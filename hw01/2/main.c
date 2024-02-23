44a5e91e6fdeed474bfa3fc77695612b3f036ed4
oyunoleg <oyun.oleg@gmail.com>
2024-02-23 23:54:54 +0700

    Homework-01

---
+++ hw01/2/main.c
@@ -0,0 +1,15 @@
+/*
+2. Вывести на экран текст "лесенкой"
+     Let’s
+       go
+            to walk
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\t Let’s\n");
+    printf("\t\tgo\n");
+    printf("\t\t\tto walk\n");
+    return 0;
+}
---
+++ hw01/3/main.c
@@ -0,0 +1,21 @@
+/*
+3.  Вывести на экран рисунок из букв
+     *
+    ***
+   *****
+  *******
+   HH HH
+   ZZZZZ
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("   HH HH\n");
+    printf("   ZZZZZ\n");
+    return 0;
+}
