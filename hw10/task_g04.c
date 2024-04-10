276cdb3820f755dc249a135755043298cc91677d
oyunoleg <oyun.oleg@gmail.com>
2024-04-10 19:18:06 +0700

    Homework-10.G04

--- hw10/task_g04.c
+++ hw10/task_g04.c
@@ -5,7 +5,7 @@ G4 ДЗ 1
 Найдите только те символы слов, которые встречаются в обоих словах только один раз.
 Напечатайте их через пробел в файл .txt в лексикографическом порядке.
 
-Данные на входе:	Два слова из маленьких английских букв через пробел. 
+Данные на входе:	Два слова из маленьких английских букв через пробел.
 Длинна каждого слова не больше 100 символов.
 Данные на выходе: 	Маленькие английские буквы через пробел.
 
@@ -56,60 +56,42 @@ void sort_str(char str[])
     }
 }
 
-// Возвращает 1 если символ уже есть в массиве иначе 0.
-int is_exists(int size, char arr[], char c)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        if (arr[i] == c)
-            return 1;
-    }
-    return 0;
-}
-
-/* Разделяет массив in на 2 слова и заполняет массив out символами из in,
-которые встречаются в обоих словах только один раз.*/
+/* Заполняет массив out символами из in, которые встречаются в обоих словах только один раз.*/
 void process(char in[], char out[])
 {
-    int in_len = strlen(in);
+    int len = strlen(in);
     int out_len = 0;
 
     // Индекс первого пробела (между словами)
-    int space_index = -1;
-    for (int i = 0; i < in_len; ++i)
-    {
-        if (in[i] == ' ')
-        {
-            space_index = i;
-            break;
-        }
-    }
+    int space_index = 0;
+    while (space_index < len && in[space_index] != ' ')
+        space_index++;
 
-    // Подсчет каждого символа в каждом слове
+    // Подсчет каждого символа в каждом слове, по первому слову
     int count1, count2;
-    for (int i = 0; i < in_len; ++i)
+    for (int i = 0; i < space_index; ++i)
     {
         count1 = 0;
         count2 = 0;
-        // Количество таких символов в первом слове
-        for (int j = 0; j < space_index; ++j)
+
+        // Количество таких же символов в первом слове
+        for (int j = 0; j < space_index && count1 <= 2; ++j)
         {
             if (in[j] == in[i])
                 count1++;
         }
 
-        // Количество таких символов во втором слове
-        for (int j = space_index + 1; j < in_len; ++j)
+        // Количество таких же символов во втором слове
+        for (int j = space_index + 1; j < len && count2 <= 2; ++j)
         {
             if (in[j] == in[i])
                 count2++;
         }
 
-        // Если символ один в каждом слове то добавляем его в результат, если его еще там нет
-        if (count1 == 1 && count2 == 1 && is_exists(out_len, out, in[i]) != 1)
+        if (count1 == 1 && count2 == 1)
             out[out_len++] = in[i];
     }
-    out[out_len] = '\0'; // теперь это строка
+    out[out_len] = '\0';
 
     // Сортируем строку
     sort_str(out);
@@ -118,21 +100,19 @@ void process(char in[], char out[])
 int main(void)
 {
     FILE *fp;
-    char line[MAX_LENGTH * 2] = {0}, result[MAX_LENGTH] = {0};
+    char str[MAX_LENGTH * 2] = {0}, result[MAX_LENGTH] = {0};
 
-    // Чтение строки из входного файла в line массив
     if ((fp = try_open_file(INPUT_FILE, "r")) == NULL)
         return 1;
-    fscanf(fp, "%[^\n]", line);
+    fscanf(fp, "%[^\n]", str);
     fclose(fp);
 
     // Получение результата в массив result
-    process(line, result);
+    process(str, result);
 
-    // Запись result в выходной файл
     if ((fp = try_open_file(OUTPUT_FILE, "w")) == NULL)
         return 1;
-    
+
     for (int i = 0; i < strlen(result); ++i)
         fprintf(fp, "%c ", result[i]);
     fclose(fp);
