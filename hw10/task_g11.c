5622b86b633602fbadf75c29b9f5177ab7546a8a
oyunoleg <oyun.oleg@gmail.com>
2024-04-10 17:14:30 +0700

    Homework-10.G11

--- hw10/task_g11.c
+++ hw10/task_g11.c
@@ -19,36 +19,47 @@ G11
 const char *INPUT_FILE = "input.txt";
 const char *OUTPUT_FILE = "output.txt";
 
+FILE *try_open_file(const char filename[], const char mode[])
+{
+    FILE *fp = fopen(filename, mode);
+    if (fp == NULL)
+    {
+        char message[1001];
+        snprintf(message, sizeof(message), "Error opening file '%s' mode '%s'", filename, mode);
+        perror(message);
+    }
+    return fp;
+}
+
+// Подсчет - сколько слов заканчиваются на букву 'а'.
+int count_words(char str[], char pattern)
+{
+    int count = 0;
+    char *word = strtok(str, " ");
+    while (word != NULL)
+    {
+        if (word[strlen(word) - 1] == pattern)
+            count++;
+        word = strtok(NULL, " ");
+    }
+    return count;
+}
+
 int main(void)
 {
     FILE *fp;
     char str[MAX_LENGTH] = {0};
 
-    // Чтение строки из входного файла в массив
-    if ((fp = fopen(INPUT_FILE, "r")) == NULL)
-    {
-        perror("Error occured while opening input file!");
+    if ((fp = try_open_file(INPUT_FILE, "r")) == NULL)
         return 1;
-    }
     fscanf(fp, "%[^\n]", str);
     fclose(fp);
 
     // Подсчет - сколько слов заканчиваются на букву 'а'.
-    int count = 0;
-    char *word = strtok(str, " ");
-    while (word != NULL)
-    {
-        if (word[strlen(word) - 1] == 'a')
-            count++;
-        word = strtok(NULL, " ");
-    }
+    int count = count_words(str, 'a');
 
-    // Запись в выходной файл
-    if ((fp = fopen(OUTPUT_FILE, "w")) == NULL)
-    {
-        perror("Error occured while opening output file!");
+    if ((fp = try_open_file(OUTPUT_FILE, "w")) == NULL)
         return 1;
-    }
     fprintf(fp, "%d", count);
     fclose(fp);
 
